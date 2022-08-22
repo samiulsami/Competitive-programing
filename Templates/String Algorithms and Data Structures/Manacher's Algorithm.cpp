5a96d024fec5e1cbaa3c95de0088e61601fb2483
samiulsami <samiulsami7786@gmail.com>
2022-08-22 06:12:45 +0600

    kickstart 2022 problems

--- Templates/String Algorithms and Data Structures/Manacher's Algorithm.cpp
+++ Templates/String Algorithms and Data Structures/Manacher's Algorithm.cpp
@@ -1,7 +1,9 @@
 void manacher(const string &s){
 	int n = s.length();
 	vector<int>odd(n),even(n);
-	///even[i] stores the value for the right center of even length palindromes
+	///odd[i] =  radius of the longest odd palindrome centered at i (indices i-odd[i]+1 to i+odd[i]-1 inclusive)
+	///even[i] = radius of the longest even palindrome centered at index i and i-1 (indices i-even[i] to i+even[i]-1 inclusive)
+	///even[i] is 0 if no even palindrome is centered at index i
 	for(int i=0,l=0,r=-1,k; i<n; i++){
 		k=(i>r)?1:min(odd[l+r-i],r-i+1);
 		while(i-k>=0 && i+k<n && s[i-k]==s[i+k])k++;
